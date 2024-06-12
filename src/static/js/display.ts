a9b6a7916f07ec1a4d429852522badd462f65cb7
CAZtheArcher <caz1158@g.rit.edu>
2024-06-12 12:04:28 -0400

    all HTML elements that are changing are added to display.ts

--- src/static/js/display.ts
+++ src/static/js/display.ts
@@ -4,4 +4,18 @@
 //    this page will also have a text box allowing the user to submit a ruling via the "https://api.krcg.org/submit-ruling/{card from list}
 //    and another one for the link to official ruling from a rules director. 
 
+//div element(s)
+const cardSearchRegion = document.querySelector('#cardSearch') as HTMLDivElement;
+const searchResultsRegion = document.querySelector('#searchDisplay') as HTMLDivElement;
+const cardDisplayRegion = document.querySelector('#cardDisplay') as HTMLDivElement;
+//button element(s)
+const cardSearchButton = document.querySelector('#cardSearchButton') as HTMLButtonElement;
+const rulingSubmissionButton = document.querySelector('#rulingSubmissionButton') as HTMLButtonElement;
+//input element(s)
 const cardSearchInput = document.querySelector('#cardSearch') as HTMLInputElement;
+const rulingInput = document.querySelector('#rulingSubmission') as HTMLInputElement;
+const linkInput = document.querySelector('#linkSubmission') as HTMLInputElement;
+//list element(s)
+const searchResults = document.querySelector('#searchResults') as HTMLUListElement;
+//p element(s)
+const cardDisplay = document.querySelector('#displayCard') as HTMLParagraphElement;
